424856e6d11dca8265fc31cd5aa4e9695defdbe5
Maksym-Plutenko <plutenkom@gmail.com>
2023-04-07 23:32:14 +0300

    no persisted store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { PersistGate } from 'redux-persist/integration/react';
+// import { PersistGate } from 'redux-persist/integration/react';
 import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store, persistor } from './redux/store';
+// import { store, persistor } from './redux/store';
+import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
         <App />
-      </PersistGate>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
